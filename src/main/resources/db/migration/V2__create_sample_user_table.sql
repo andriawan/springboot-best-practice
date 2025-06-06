88c9eb281af81ebc3a848b256d5d2dea4c54ce69
Muhammad Irwan Andriawan <irwan@andriawan.com>
2025-06-06 11:14:11 +0000

    chore(flyway): add dependency flyway. init base config sql script, properties and compose file

---
+++ src/main/resources/db/migration/V2__create_sample_user_table.sql
@@ -0,0 +1,11 @@
+INSERT INTO users (name, password, created_at, updated_at, is_active, is_deleted) VALUES
+('Alice Johnson', '$2b$12$KIXVm39ivZzK8oIucLmR1u2sLfQxiyvLd6JfWn6vUMuKmpVDwIDva', NOW(), NOW(), TRUE, FALSE),
+('Bob Smith', '$2b$12$KIXVm39ivZzK8oIucLmR1u2sLfQxiyvLd6JfWn6vUMuKmpVDwIDva', NOW(), NOW(), TRUE, FALSE),
+('Charlie Davis', '$2b$12$KIXVm39ivZzK8oIucLmR1u2sLfQxiyvLd6JfWn6vUMuKmpVDwIDva', NOW(), NOW(), TRUE, FALSE),
+('Diana Moore', '$2b$12$KIXVm39ivZzK8oIucLmR1u2sLfQxiyvLd6JfWn6vUMuKmpVDwIDva', NOW(), NOW(), TRUE, FALSE),
+('Edward Lee', '$2b$12$KIXVm39ivZzK8oIucLmR1u2sLfQxiyvLd6JfWn6vUMuKmpVDwIDva', NOW(), NOW(), TRUE, FALSE),
+('Fiona Walker', '$2b$12$KIXVm39ivZzK8oIucLmR1u2sLfQxiyvLd6JfWn6vUMuKmpVDwIDva', NOW(), NOW(), TRUE, FALSE),
+('George Allen', '$2b$12$KIXVm39ivZzK8oIucLmR1u2sLfQxiyvLd6JfWn6vUMuKmpVDwIDva', NOW(), NOW(), TRUE, FALSE),
+('Hannah Scott', '$2b$12$KIXVm39ivZzK8oIucLmR1u2sLfQxiyvLd6JfWn6vUMuKmpVDwIDva', NOW(), NOW(), TRUE, FALSE),
+('Isaac Young', '$2b$12$KIXVm39ivZzK8oIucLmR1u2sLfQxiyvLd6JfWn6vUMuKmpVDwIDva', NOW(), NOW(), TRUE, FALSE),
+('Julia Baker', '$2b$12$KIXVm39ivZzK8oIucLmR1u2sLfQxiyvLd6JfWn6vUMuKmpVDwIDva', NOW(), NOW(), TRUE, FALSE);
