72fcbc43a247a880c39c6b88c17661cc68b9777e
Muhammad Irwan Andriawan <irwan@andriawan.com>
2025-06-06 16:24:52 +0000

    feat(jwt): config jwt based auth

---
+++ src/main/java/com/andriawan/andresource/config/Security.java
@@ -0,0 +1,81 @@
+package com.andriawan.andresource.config;
+
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class Security {
+
+  @Value("${rsa.key.private}")
+  private RSAPrivateKey rsaPrivateKey;
+
+  @Value("${rsa.key.public}")
+  private RSAPublicKey rsaPublicKey;
+
+  @Value("${auth.sample.user}")
+  private String sampeUsername;
+
+  @Value("${auth.sample.password}")
+  private String samplePassword;
+
+  private String[] publicRoute = {
+    "/api/v1/auth/login",
+    "/api/v1/auth/token/refresh"
+  };
+
+  @Bean
+  public InMemoryUserDetailsManager sampleUser() {
+    UserDetails user = User.withUsername(sampeUsername)
+      .password("{noop}".concat(samplePassword)).build();
+    return new InMemoryUserDetailsManager(user);
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http.httpBasic(Customizer.withDefaults())
+        .authorizeHttpRequests(
+            request ->
+                request.requestMatchers(publicRoute).permitAll().anyRequest().authenticated())
+        .csrf(crsf -> crsf.disable())
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .build();
+  }
+
+  @Bean
+  public JwtDecoder JwtDecoder() {
+    return NimbusJwtDecoder.withPublicKey(rsaPublicKey).build();
+  }
+
+  @Bean
+  public JwtEncoder jwtEncoder() {
+    JWK jwk = new RSAKey.Builder(rsaPublicKey).privateKey(rsaPrivateKey).build();
+    JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
+    return new NimbusJwtEncoder(jwkSource);
+  }
+}
---
+++ src/main/java/com/andriawan/andresource/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.andriawan.andresource.controller;
+
+import com.andriawan.andresource.service.TokenService;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+  @Autowired private TokenService tokenService;
+
+  @PostMapping("/login")
+  public ResponseEntity<Map<String, String>> authenticate(Authentication authentication) {
+    return ResponseEntity.ok(tokenService.generateToken(authentication));
+  }
+
+  @PostMapping("/token/refresh")
+  public ResponseEntity<Map<String, String>> refreshToken(Authentication authentication) {
+    return ResponseEntity.ok(tokenService.generateToken(authentication));
+  }
+}
