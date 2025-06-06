954b48315d62d54b2b4c11786acb1ccc48e58174
Muhammad Irwan Andriawan <irwan@andriawan.com>
2025-06-06 02:15:05 +0000

    feat(init): init repo

---
+++ src/main/java/com/andriawan/andresource/AndresourceApplication.java
@@ -0,0 +1,13 @@
+package com.andriawan.andresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndresourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndresourceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andriawan/andresource/AndresourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriawan.andresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndresourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
