692638df19b2fc967398b248a5ceb80dfa618134
Muhammad Irwan Andriawan <irwan@andriawan.com>
2025-06-06 05:14:48 +0000

    chore(spotless): add spotless maven plugin as code formatter

--- src/main/java/com/andriawan/andresource/AndresourceApplication.java
+++ src/main/java/com/andriawan/andresource/AndresourceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AndresourceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AndresourceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(AndresourceApplication.class, args);
+  }
 }
--- src/test/java/com/andriawan/andresource/AndresourceApplicationTests.java
+++ src/test/java/com/andriawan/andresource/AndresourceApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AndresourceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
