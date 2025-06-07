dc3a003a46f909d810f332cd6578590781246f54
Muhammad Irwan Andriawan <irwan@andriawan.com>
2025-06-07 13:53:09 +0000

    feat(execption-handling): make auth exception as json response

--- src/main/java/com/andriawan/andresource/controller/AuthController.java
+++ src/main/java/com/andriawan/andresource/controller/AuthController.java
@@ -3,8 +3,11 @@ package com.andriawan.andresource.controller;
 import com.andriawan.andresource.service.TokenService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import java.util.Map;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +15,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@Slf4j
 public class AuthController {
   @Autowired private TokenService tokenService;
 
   @SecurityRequirement(name = "basicAuth")
   @PostMapping("/login")
   public ResponseEntity<Map<String, String>> authenticate(Authentication authentication) {
-    return ResponseEntity.ok(tokenService.generateToken(authentication));
+    return Optional.ofNullable(authentication)
+        .map(auth -> ResponseEntity.ok(tokenService.generateToken(auth)))
+        .orElseThrow(
+            () ->
+                new BadCredentialsException(
+                    "Please supply auth basic method. see detail at https://datatracker.ietf.org/doc/html/rfc7617"));
   }
 
   @SecurityRequirement(name = "jwt")
