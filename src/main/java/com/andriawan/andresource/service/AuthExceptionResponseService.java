b0bab2a7bb30de8d606e5765a2da24ca04a39bad
Muhammad Irwan Andriawan <irwan@andriawan.com>
2025-06-27 07:20:41 +0000

    refactor: simplify logic

--- src/main/java/com/andriawan/andresource/service/AuthExceptionResponseService.java
+++ src/main/java/com/andriawan/andresource/service/AuthExceptionResponseService.java
@@ -1,12 +1,10 @@
 package com.andriawan.andresource.service;
 
-import com.nimbusds.jwt.proc.BadJWTException;
 import java.util.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -21,40 +19,30 @@ public class AuthExceptionResponseService {
 
   private final Map<String, AuthExceptionMessageResolver> resolverMap = new HashMap<>();
 
+  private Response badRequestResponse(AuthenticationException authenticationException) {
+    return badRequestResponse(authenticationException, authenticationException.getMessage());
+  }
+
+  private Response badRequestResponse(
+      AuthenticationException authenticationException, String message) {
+    return new Response(
+        message, HttpStatus.BAD_REQUEST.value(), authenticationException.getClass().getName());
+  }
+
+  private Response unauthorizeResponse(AuthenticationException authenticationException) {
+    return new Response(
+        authenticationException.getMessage(),
+        HttpStatus.UNAUTHORIZED.value(),
+        authenticationException.getClass().getName());
+  }
+
   public AuthExceptionResponseService() {
+    resolverMap.put("default", exception -> unauthorizeResponse(exception));
     resolverMap.put(
-        "default",
-        exception ->
-            new Response(
-                exception.getMessage(),
-                HttpStatus.UNAUTHORIZED.value(),
-                exception.getClass().getName()));
-    resolverMap.put(
-        BadCredentialsException.class.getName(),
-        exception ->
-            new Response(
-                exception.getMessage(),
-                HttpStatus.BAD_REQUEST.value(),
-                exception.getClass().getName()));
+        BadCredentialsException.class.getName(), exception -> badRequestResponse(exception));
     resolverMap.put(
         InsufficientAuthenticationException.class.getName(),
-        exception ->
-            new Response(
-                "Invalid Credentials",
-                HttpStatus.BAD_REQUEST.value(),
-                exception.getClass().getName()));
-    resolverMap.put(
-        AuthenticationException.class.getName(),
-        exception -> {
-          Throwable cause = exception.getCause();
-          String message = "";
-          if (cause instanceof BadJWTException || cause instanceof JwtException) {
-            message = cause.getMessage().toLowerCase();
-            message = message.contains("expired") ? "Token expired" : "Invalid token";
-          }
-          return new Response(
-              message, HttpStatus.UNAUTHORIZED.value(), exception.getClass().getName());
-        });
+        exception -> badRequestResponse(exception, "Invalid Credentials"));
   }
 
   public Response getMappedResponse(AuthenticationException exception) {
