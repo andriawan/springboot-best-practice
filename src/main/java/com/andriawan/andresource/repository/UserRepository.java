578317b9ccddffc7856e26ea9d6c3d248edb476b
Muhammad Irwan Andriawan <irwan@andriawan.com>
2025-06-27 16:19:43 +0000

    chore: add more jpa query user

--- src/main/java/com/andriawan/andresource/repository/UserRepository.java
+++ src/main/java/com/andriawan/andresource/repository/UserRepository.java
@@ -1,9 +1,20 @@
 package com.andriawan.andresource.repository;
 
 import com.andriawan.andresource.entity.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
+
+  Optional<User> findByIdAndIsDeletedFalse(Long id);
+
+  boolean existsByEmailAndIsDeletedFalse(String email);
+
+  Optional<User> findByEmailAndIsDeletedFalse(String email);
+
+  List<User> findAllByIsDeletedFalse();
+
+  List<User> findAllByIsDeletedFalseAndIsActive(boolean isActive);
 }
