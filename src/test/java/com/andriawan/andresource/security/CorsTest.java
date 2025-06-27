cd2ac4d5a3aaa5ba7980f40087c4c997d1a23d6f
Muhammad Irwan Andriawan <irwan@andriawan.com>
2025-06-27 03:54:52 +0000

    chore: add test for cors spring security

---
+++ src/test/java/com/andriawan/andresource/security/CorsTest.java
@@ -0,0 +1,58 @@
+package com.andriawan.andresource.security;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class CorsTest {
+  @ServiceConnection
+  static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+
+  @Autowired private WebTestClient webTestClient;
+
+  private static final String ORIGIN = "http://localhost:3000";
+
+  @DynamicPropertySource
+  static void overrideProperties(DynamicPropertyRegistry registry) {
+    registry.add("cors.enabled", () -> "true");
+    registry.add("cors.origin.allowed", () -> ORIGIN);
+    registry.add("cors.method.allowed", () -> "GET");
+  }
+
+  @BeforeAll
+  static void beforeAll() {
+    postgres.start();
+  }
+
+  @AfterAll
+  static void afterAll() {
+    postgres.stop();
+  }
+
+  @Test
+  void shouldAllowCorsPreflightRequest() {
+    webTestClient
+        .options()
+        .uri("/actuator")
+        .header(HttpHeaders.ORIGIN, ORIGIN)
+        .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "GET")
+        .exchange()
+        .expectStatus()
+        .isOk()
+        .expectHeader()
+        .valueEquals(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, ORIGIN)
+        .expectHeader()
+        .valuesMatch(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET");
+  }
+}
