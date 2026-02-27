28e017e7a2ba8911142ceeb88422b9e203e8413b
Muhammad Irwan Andriawan <irwan@andriawan.com>
2026-02-27 15:46:46 +0000

    refactor: migration to springboot 4

--- src/test/java/com/andriawan/andresource/repository/UserJpaTest.java
+++ src/test/java/com/andriawan/andresource/repository/UserJpaTest.java
@@ -15,12 +15,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.cache.test.autoconfigure.AutoConfigureCache;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 @DataJpaTest
+@AutoConfigureCache
 @ActiveProfiles("test")
 class UserJpaTest {
 
